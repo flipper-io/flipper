ec1b2bba0cc7115e9fd8f1b79bda5e0cc81215c0
Travis Whitaker <pi.boy.travis@gmail.com>
2015-11-26 00:02:52 -0500

    Make x86_64 internal_call work whether or not it is called with a 16 byte aligned stack.

--- libflipper/architectures/x86_64/internal_call.asm
+++ libflipper/architectures/x86_64/internal_call.asm
@@ -13,18 +13,25 @@ internal_call:	push rbp         ; Save caller base pointer.
 		push rbx         ; Save caller's rbx;
 		push r12         ; Save caller's r12.
 		push r13         ; Save caller's r13.
+		push r14         ; Save caller's r14.
 		mov r10, rdi     ; Save addr
 		mov r11, rsi     ; Save argc
 		mov r12, rdx     ; Save argv
+		mov rdx, rsp
+		and rdx, 0xf     ; rsp % 16
+		neg rdx
+		add rdx, 16
+		mov r13, rdx     ; Save number of bytes added to stack.
+		sub rsp, rdx     ; Align the stack.
 		mov rax, 0       ; rax will buffer the arguments.
 		cmp r11, 6       ; argc >= 6?
 		jae arg6         ; If so, we're moving all 6 args.
-		mov r13, 0       ; r13 is the jump table offset.
-		sub r13, r11     ; Subtract the number of arguments.
-		shl r13, 2       ; r13 * 12 = (r13 << 2) * 3
-		lea r13, [r13*3]
+		mov r14, 0       ; r14 is the jump table offset.
+		sub r14, r11     ; Subtract the number of arguments.
+		shl r14, 2       ; r14 * 12 = (r14 << 2) * 3
+		lea r14, [r14*3]
 		lea rbx, [rel arg0]
-		lea rbx, [rbx + r13]
+		lea rbx, [rbx + r14]
 		jmp rbx          ; Jump into the table.
 arg6:		mov ax, [r12 + 5*2]
 		mov r9, rax
@@ -45,22 +52,22 @@ arg1:		mov ax, [r12]
 		mov rdi, rax
 		dec r11
 		nop
-arg0:		mov r13, 0   ; Clear the stack block count.
-		cmp r11, 0   ; Are we done handling arguments?
+arg0:		cmp r11, 0   ; Are we done handling arguments?
 		je subcall   ; If so, do the call.
 		test r11, 1  ; Check if number of args left is odd.
 		jnz argstack ; If it's odd, we don't need to align rsp.
 		sub rsp, 8   ; If it's even, keep the stack 16 byte aligned.
-		inc r13      ; Increment stack block flag.
+		add r13, 8   ; Increment stack block flag.
 argstack:	mov ax, [r12 + 10 + 2*r11]
 		push rax
-		inc r13
+		add r13, 8
 		dec r11
 		cmp r11, 0
 		jne argstack
 subcall:	call r10       ; Perform the call.
 		mov [r12], eax ; Save the 4 byte return value at argv.
-		lea rsp, [rsp + 8*r13] ; Clean up stack frame.
+		add rsp, r13 ; Clean up stack frame.
+		pop r14
 		pop r13 ; Restore caller's r13.
 		pop r12 ; Restore caller's r12.
 		pop rbx ; Restore caller's rbx.
