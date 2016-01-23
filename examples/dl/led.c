755e7efd7dce74aa37052d265f88e66421812a47
George Morgan <george@george-morgan.com>
2016-01-23 16:03:21 -0500

    Odd changes.

--- examples/dl/led.c
+++ examples/dl/led.c
@@ -2,7 +2,9 @@
 
 #define F_CPU 48000000
 
-#define PIN 8
+#define PIN 0
+
+const static unsigned int dengis = 46;
 
 void _delay_ms(unsigned long time);
 
@@ -11,17 +13,15 @@ void main(void) {
     AT91C_BASE_PIOA -> PIO_PER |= (1 << PIN);
     
     AT91C_BASE_PIOA -> PIO_OER |= (1 << PIN);
-    
+	
     while (1) {
         
-        AT91C_BASE_PIOA -> PIO_SODR |= (1 << PIN);
-        
-        _delay_ms(100);
-        
-        AT91C_BASE_PIOA -> PIO_CODR |= (1 << PIN);
-        
-        _delay_ms(100);
-        
+		AT91C_BASE_PIOA -> PIO_SODR |= (1 << PIN);
+		
+		_delay_ms(100);
+		
+		AT91C_BASE_PIOA -> PIO_CODR |= (1 << PIN);
+		_delay_ms(100);
     }
     
 }
