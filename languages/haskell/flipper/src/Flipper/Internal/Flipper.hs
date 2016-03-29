c2eaad5c655a60d884c0b128671732cda4076f21
Travis Whitaker <pi.boy.travis@gmail.com>
2016-03-28 21:06:48 -0400

    Add some more Haskell stuff.

--- languages/haskell/flipper/src/Flipper/Internal/Flipper.hs
+++ languages/haskell/flipper/src/Flipper/Internal/Flipper.hs
@@ -11,25 +11,25 @@ data Endpoint = USB     -- ^ Local connection via USB.
               | Network -- ^ Remote connection via TCP/UDP.
               | FVM     -- ^ Flipper simulation via FVM.
 
+enumEndpoint :: Endpoint -> CInt
+enumEndpoint USB     = 0
+enumEndpoint Network = 1
+enumEndpoint FVM     = 2
+
 select :: String -> IO Bool
-select n = do
-    n' <- newCString n
-    r <- (not . toBool) <$> c_flipper_select n'
-    free n'
-    return r
+select n = withCString n (((not . toBool) <$>) . c_flipper_select)
 
 attach :: Endpoint -> String -> IO Bool
-attach e n = do
-    n' <- newCString n
-    r <- (not . toBool) <$> c_flipper_attach (enumEndpoint e) n'
-    free n'
-    return r
-    where enumEndpoint USB     = 0
-          enumEndpoint Network = 1
-          enumEndpoint FVM     = 2
+attach e n = withCString n (((not . toBool) <$>) . c_flipper_attach (enumEndpoint e))
+
+detach :: String -> IO Bool
+detach n = withCString n (((not . toBool) <$>) . c_flipper_detach)
 
 foreign import ccall safe "flipper/flipper/flipper.h flipper_select"
     c_flipper_select :: CString -> IO CInt
 
 foreign import ccall safe "flipper/flipper/flipper.h flipper_attach"
     c_flipper_attach :: CInt -> CString -> IO CInt
+
+foreign import ccall safe "flipper/flipper/flipper.h flipper_detach"
+    c_flipper_detach :: CString -> IO CInt
