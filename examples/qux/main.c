240977e297edce6c1f9b43342895faf89d4a96a2
George Morgan <george@george-morgan.com>
2016-04-01 21:14:30 -0400

    Heavily revised the example module 'qux'.

--- examples/qux/main.c
+++ examples/qux/main.c
@@ -5,28 +5,28 @@
 
 /* ~ Simple program that uses the Flipper module 'qux.' ~ */
 int main(int argc, char *argv[]) {
-	
+
 	if (argc < 2) {
 		printf("\nUsage: qux [on | off]\n\n");
 		return 0;
 	}
-	
-	/* ~ Attach to a Flipper device over USB. ~ */
-	flipper.attach(FLIPPER_USB);
-	
+
+	/* ~ Attach to the first available Flipper device over USB. ~ */
+	flipper.attach();
+
 	/* ~ Configure the 'qux' module. ~ */
 	qux.configure();
-	
+
 	/* ~ Call the qux_on function if the first argument is "on". ~ */
 	if (!strcmp(argv[1], "on")) {
 		qux.on();
 	}
-	
+
 	/* ~ Otherwise, call the qux_off function if the first argument is "off". ~ */
 	else if (!strcmp(argv[1], "off")) {
 		qux.off();
 	}
-	
+
 	return 0;
-	
+
 }
