9c2b4f35f50d2be5b3a68a65c5fd591c77964adb
Travis Whitaker <pi.boy.travis@gmail.com>
2016-04-25 17:11:31 -0400

    Flesh out Put and Bufferable.

--- languages/haskell/flipper/src/Flipper/Bufferable.hs
+++ languages/haskell/flipper/src/Flipper/Bufferable.hs
@@ -2,11 +2,253 @@
 
 module Flipper.Bufferable where
 
+import Data.Complex
+import Data.Int
+import Data.Ratio
+import Data.Word
+
 import Flipper.Get
 import Flipper.Put
 
+import Foreign.C.Types
+import Foreign.Ptr
+import Foreign.StablePtr
 import Foreign.Storable
+
+import System.Posix.Types
 
 class Bufferable a where
     put :: a -> Put
     get :: Get a
+
+instance Bufferable Bool where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Char where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Double where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Float where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Int where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Int8 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Int16 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Int32 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Int64 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Word where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Word8 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Word16 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Word32 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Word64 where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUIntMax where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CIntMax where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUIntPtr where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSUSeconds where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUSeconds where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CTime where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CClock where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSigAtomic where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CWchar where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSize where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CPtrdiff where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CDouble where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CFloat where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CULLong where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CULong where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CLong where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUInt where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CInt where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUShort where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CShort where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUChar where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSChar where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CChar where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable IntPtr where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable WordPtr where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable Fd where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CRLim where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CTcflag where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSpeed where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CCc where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CUid where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CNlink where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CGid where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CSsize where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CPid where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable COff where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CMode where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CIno where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable CDev where
+    put = putStorable
+    get = getStorable
+
+instance (Storable a, Integral a) => Bufferable (Ratio a) where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable (StablePtr a) where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable (Ptr a) where
+    put = putStorable
+    get = getStorable
+
+instance Bufferable (FunPtr a) where
+    put = putStorable
+    get = getStorable
+
+instance Storable a => Bufferable (Complex a) where
+    put = putStorable
+    get = getStorable
--- languages/haskell/flipper/src/Flipper/Put.hs
+++ languages/haskell/flipper/src/Flipper/Put.hs
@@ -1,14 +1,19 @@
 module Flipper.Put where
 
+import qualified Data.ByteString       as B
+import qualified Data.ByteString.Char8 as C
+
+import qualified Data.Text             as T
+import qualified Data.Text.Encoding    as T
+
 import Data.Word
 
 import Flipper.Buffer
 
 import Foreign.ForeignPtr
+import Foreign.Marshal.Utils
 import Foreign.Ptr
 import Foreign.Storable
-
-import System.IO.Unsafe
 
 data Put = Put (Ptr Word8 -> IO ()) -- Buffer writer.
                Int                  -- Number of bytes contributed.
@@ -18,10 +23,37 @@ instance Monoid Put where
     mappend (Put w1 l1) (Put w2 l2) = Put w' (l1 + l2)
         where w' p = w1 p >> w2 (p `plusPtr` l1)
 
-runPut :: Put -> Buffer
-runPut (Put w l) = let b@(Buffer p _ _) = unsafeAllocBuffer l
-                       r                = unsafeDupablePerformIO $ withForeignPtr p w
-                   in seq r b
+runPut :: Put -> IO Buffer
+runPut (Put w l) = do b@(Buffer p _ _) <- allocBuffer l
+                      withForeignPtr p w
+                      return b
 
+putBuffer :: Buffer -> Put
+putBuffer (Buffer p o l) = Put w l
+    where w d = withForeignPtr p $ \p' -> copyBytes (castPtr d) (castPtr p') l
+
+-- | 'Put' a 'Buffer' like C would, with a null terminator.
+putBufferC :: Buffer -> Put
+putBufferC = (<> putStorable (0 :: Word8)) . putBuffer
+
+-- | 'Put' a 'Buffer' like Pascal would, with a preceeding 32-bit word
+--   indicating the length.
+putBufferPascal :: Buffer -> Put
+putBufferPascal b@(Buffer _ _ l) = putStorable ((fromIntegral l) :: Word32) <> putBuffer b
+
+-- | 'Put' for any natively marshalable data.
 putStorable :: Storable a => a -> Put
 putStorable s = Put (\p -> poke (castPtr p) s) (sizeOf s)
+
+-- | Uses ASCII encoding. This forces the entire string into memory. Must not
+--   contain NULL.
+putString :: String -> Put
+putString = putBufferC . fromByteString . C.pack
+
+-- | Uses UTF8 encoding. Must not contain NULL.
+putText :: T.Text -> Put
+putText = putBufferC . fromByteString . T.encodeUtf8
+
+-- | May contain NULL. Must be less than 2^32 bytes long.
+putByteString :: B.ByteString -> Put
+putByteString = putBufferPascal . fromByteString
