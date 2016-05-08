77ecfccc9f0875bb7107cb02349207b52619f309
Travis Whitaker <pi.boy.travis@gmail.com>
2016-05-08 17:23:20 -0400

    Add the Flipper monad and provide an LED interface.

---
+++ languages/haskell/flipper/src/Flipper/LED.hs
@@ -0,0 +1,19 @@
+{-|
+Module      : Flipper.LED
+Description : Control Flipper's Status LED.
+Copyright   : George Morgan, Travis Whitaker 2016
+License     : All rights reserved.
+Maintainer  : travis@flipper.io
+Stability   : Provisional
+Portability : Windows, POSIX
+
+-}
+
+module Flipper.LED where
+
+import Flipper.MonadFlipper
+
+import qualified Flipper.Internal.LED as I
+
+setRGB :: MonadFlipper m => I.RGB -> m ()
+setRGB = bracketIO . I.setRGB
