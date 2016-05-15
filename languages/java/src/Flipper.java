881295128f6a2d100fa492b9e884464e09f2731c
Nick Mosher <nicholastmosher@gmail.com>
2016-05-14 20:17:01 -0400

    Added error handling to Java bindings.

--- languages/java/src/Flipper.java
+++ languages/java/src/Flipper.java
@@ -6,72 +6,78 @@ import com.sun.jna.Native;
  */
 public class Flipper {
 
-    private static _flipper FLIPPER;
-
     public static final int USB = 0;
     public static final int NETWORK = 1;
     public static final int FVM = 2;
 
-    public interface _flipper extends Library {
+    private static _binding BINDING;
 
+    interface _binding extends Library {
+
+        //Flipper attach driver bindings.
         int flipper_attach();
         int flipper_attach_name(int endpoint, String name);
         int flipper_select(String name);
         int flipper_detach(String name);
 
+        //Flipper error driver bindings.
+        void error_configure();
+        void error_withold();
+        void error_disclose();
+        void error_raise(short code, String message);
+        short error_get();
+        void error_clear();
+
+        //Flipper io driver bindings.
         void io_configure();
         void io_set_direction(byte pin, byte direction);
         void io_write(byte pin, short value);
         short io_read(byte pin);
 
+        //Flipper button driver bindings.
         void button_configure();
         short button_read();
 
+        //Flipper led driver bindings.
         void led_configure();
         void led_set_rgb(byte red, byte green, byte blue);
     }
 
-    private _flipper getFLIPPER() {
-        if(FLIPPER == null) {
-            FLIPPER = (_flipper) Native.loadLibrary("flipper", _flipper.class);
+    /**
+     * Default-access method allows driver-wrapping classes in this package to get
+     * the instance of the Flipper bindings and execute them, but no external-scope
+     * methods can.
+     * @return An inflated interface of Java methods bound to underlying C functions.
+     */
+    _binding getBinding() {
+        if(BINDING == null) {
+            BINDING = (_binding) Native.loadLibrary("flipper", _binding.class);
         }
-        return FLIPPER;
+        return BINDING;
     }
 
-    public Io io;
-    public Button button;
-    public Led led;
+    /*
+     * Instantiate each driver wrapper.
+     */
+    public final Io io = new Io(this);
+    public final Button button = new Button(this);
+    public final Led led = new Led(this);
+    public final Error error = new Error(this);
 
+    /**
+     * Constructs a Flipper object bound to a Flipper with the given name
+     * over the given endpoint (USB, NETWORK, or FVM (Flipper Virtual Machine)).
+     * @param endpoint The communication endpoint to connect to Flipper with.
+     * @param name The name of the Flipper device to connect to.
+     */
     public Flipper(int endpoint, String name) {
-        getFLIPPER().flipper_attach_name(endpoint, name);
-        io = new Io(FLIPPER);
+        getBinding().flipper_attach_name(endpoint, name);
     }
 
     /**
      * Constructs an instance of Flipper.
      */
     public Flipper() {
-        getFLIPPER().flipper_attach();
-        io = new Io(FLIPPER);
-        button = new Button(FLIPPER);
-        led = new Led(FLIPPER);
-    }
-
-    public static void main(String[] args) {
-        Flipper flipper = new Flipper();
-        flipper.io.direction(1, true);
-        flipper.io.write(1, true);
-        flipper.led.setRGB((byte) 0, (byte) 0, (byte) 10);
-
-        int value = 0;
-        while(true) {
-            //flipper.led.setRGB((byte) (value % 255), (byte) ((value + 50) % 255), (byte) ((value + 100) % 255));
-            //value++;
-            System.out.println("Button: " + (flipper.button.read() ? "Pressed" : "Released"));
-            if(flipper.button.read()) flipper.led.setRGB(10, 0, 0); else flipper.led.setRGB(0, 10, 0);
-            try {
-                Thread.sleep(10);
-            } catch(Exception e) { }
-        }
+        getBinding().flipper_attach();
     }
 }
