32463d7d6e3587456583c3f9be609e7607f31c47
Travis Whitaker <pi.boy.travis@gmail.com>
2016-05-29 18:55:08 -0400

    Add missing qux driver folder, add .a and .so to gitignore.

---
+++ examples/qux/qux/qux.c
@@ -0,0 +1,26 @@
+#define __private_include__
+#include "qux.h"
+
+/* Create a handle for the module. */
+static fmr_module handle;
+
+void qux_configure(void) {
+
+	/* Registers this module with the Flipper Message Runtime. Also calls the remote configure function. */
+	handle = fmr.bind("io.flipper.qux");
+
+}
+
+void qux_on(void) {
+
+	/* Invokes the function 'qux_on' on the device. */
+	fmr.invoke(handle, _qux_on, NO_ARGS);
+
+}
+
+void qux_off(void) {
+
+	/* Invokes the function 'qux_off' on the device. */
+	fmr.invoke(handle, _qux_off, NO_ARGS);
+
+}
