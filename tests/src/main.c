f8a5bf3c5fef2d1b40cfde06199a1ade6534d8b2
George Morgan <george@george-morgan.com>
2016-09-26 21:22:38 -0400

    Switched back to LED test.

--- tests/src/main.c
+++ tests/src/main.c
@@ -9,28 +9,24 @@ int main(int argc, char *argv[]) {
 	//flipper_attach_endpoint("fvm", &lf_fvm_ep);
 	flipper_attach();
 
+	if (argc < 4) {
+		error_raise(E_NULL, "Invalid number of arguments provided. Expected R, G, B.");
+		return EXIT_FAILURE;
+	}
+
 	/* Create an empty message runtime module instance. */
 	struct _fmr_module _lf_led_module;
 	_lf_led_module.device = flipper.device;
-
-	struct _fmr_module _lf_button_module;
-	_lf_button_module.device = flipper.device;
 
 	/* Bind the empty instance to the default 'led' module which comes pre-loaded on all devices. */
 	int _e = fmr_bind(&_lf_led_module, "_lf_led");
 	if (_e < lf_success) {
 		error_raise(E_MODULE, "Failed to bind to LED module.");
 	}
-	_lf_led_module.identifier = 1;
-
-	_e = fmr_bind(&_lf_button_module, "_lf_button");
-	if (_e < lf_success) {
-		error_raise(E_MODULE, "Failed to bind to button module.");
-	}
-	_lf_button_module.identifier = 0;
+	_lf_led_module.identifier = 0;
 
 	/* Perform the function invocation. */
-	uint32_t result = lf_invoke(&_lf_button_module, _button_read, fmr_build(0));
+	uint32_t result = lf_invoke(&_lf_led_module, _led_set_rgb, fmr_args(fmr_int8(atoi(argv[1])), fmr_int8(atoi(argv[2])), fmr_int8(atoi(argv[3]))));
 	printf("The value was 0x%08x.\n", result);
 
     return 0;
