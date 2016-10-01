48ba7f5931fa7296449ca590cdcffef373cbb423
Travis Whitaker <pi.boy.travis@gmail.com>
2016-09-30 21:44:27 -0700

    Update console actions and exec.

--- console/src/Flipper/Console/Action.hs
+++ console/src/Flipper/Console/Action.hs
@@ -2,14 +2,8 @@ module Flipper.Console.Action where
 
 import Data.Word
 
-import Flipper.NVM
-import Flipper.FS
-import Flipper.IO
-import Flipper.LED
-import Flipper.USART
-
 data ConsoleAction = Flash FilePath
-                   | Install BundleID FilePath
+                   | Install ModuleID FilePath
                    | Launch String
                    | Reset
                    | Suspend
@@ -18,60 +12,48 @@ data ConsoleAction = Flash FilePath
                    | ConsoleCall Call
                    deriving (Eq, Ord, Show)
 
-newtype BundleID = BundleID { unBundleID :: [String] }
+newtype ModuleID = ModuleID { unModuleID :: [String] }
                  deriving (Eq, Ord, Show)
 
 data Call = ButtonCall ButtonAction
-          | ConfigCall ConfigAction
-          | IOCall IOAction
+          | FSCall FSAction
+          | GPIOCall GPIOAction
           | LEDCall LEDAction
-          | NVMCall NVMAction
           | SPICall SPIAction
-          | USARTCall USARTAction
-          | USBCall USBAction
+          | UARTCall UARTAction
           deriving (Eq, Ord, Show)
 
 data ButtonAction = ButtonRead
                   deriving (Eq, Ord, Show)
 
-data ConfigAction = ConfigRead Word8
-                  | ConfigWrite Word8 Word16
-                  deriving (Eq, Ord, Show)
-
-data IOAction = IODigitalDirection DigitalPin Direction
-              | IODigitalRead DigitalPin
-              | IODigitalWrite DigitalPin Bool
-              | IOAnalogDirection AnalogPin Direction
-              | IOAnalogRead AnalogPin
-              | IOAnalogWrite AnalogPin Word16
+data FSAction = FSFormat
+              | FSCreateFromString String String
+              | FSCreateFromFile String FilePath
+              | FSRemove String
+              | FSRename String String
               deriving (Eq, Ord, Show)
+
+data GPIOAction = GPIODigitalDirection DigitalPin Direction
+                | GPIODigitalRead DigitalPin
+                | GPIODigitalWrite DigitalPin Bool
+                | GPIOAnalogDirection AnalogPin Direction
+                | GPIOAnalogRead AnalogPin
+                | GPIOAnalogWrite AnalogPin Word16
+                deriving (Eq, Ord, Show)
 
 data LEDAction = LEDSetRGB RGB
                deriving (Eq, Ord, Show)
 
-data NVMAction = NVMEnable
-               | NVMDisable
-               | NVMReset
-               | NVMFormat
-               | NVMAlloc Int
-               | NVMRead FSHandle
-               | NVMWrite FSHandle String
-                deriving (Eq, Ord, Show)
-
 data SPIAction = SPIEnable
                | SPIDisable
-               | SPIRead
-               | SPIWrite String
+               | SPIRead Int
+               | SPIWriteFromString String
+               | SPIWriteFromFile FilePath
                deriving (Eq, Ord, Show)
 
-data USARTAction = USARTEnable USART
-                 | USARTDisable USART
-                 | USARTRead USART
-                 | USARTWrite USART String
-                 deriving (Eq, Ord, Show)
-
-data USBAction = USBEnable
-               | USBDisable
-               | USBRead
-               | USBWrite String
-               deriving (Eq, Ord, Show)
+data UARTAction = UARTEnable
+                | UARTDisable
+                | UARTRead Int
+                | UARTWriteFromString String
+                | UARTWriteFromFilePath String
+                deriving (Eq, Ord, Show)
--- console/src/Flipper/Console/Exec.hs
+++ console/src/Flipper/Console/Exec.hs
@@ -2,28 +2,29 @@ module Flipper.Console.Exec where
 
 import Control.Monad.Trans.Class
 
+import qualified Data.ByteString as BS
+
 import Flipper
 
 import Flipper.Error
 import Flipper.MonadFlipper
 
+import qualified Flipper.Buffer as B
 import qualified Flipper.Button as Button
-import qualified Flipper.Config as Config
-import qualified Flipper.FDL    as FDL
 import qualified Flipper.FS     as FS
-import qualified Flipper.IO     as IO
+import qualified Flipper.GPIO   as GPIO
 import qualified Flipper.LED    as LED
-import qualified Flipper.NVM    as NVM
-import qualified Flipper.SAM    as SAM
+import qualified Flipper.CPU    as CPU
 import qualified Flipper.SPI    as SPI
-import qualified Flipper.USART  as USART
-import qualified Flipper.USB    as USB
+import qualified Flipper.UART   as UART
 
 import Flipper.Console.Action
 import Flipper.Console.Parsers
 import Flipper.Console.Error
 
 import System.Console.Haskeline
+
+import System.IO
 
 import qualified Text.Megaparsec        as M
 import qualified Text.Megaparsec.String as M
@@ -40,9 +41,13 @@ printCStringFC :: Either String String -> FC ()
 printCStringFC (Left e)  = lift $ outputStrLn e
 printCStringFC (Right v) = lift $ outputStrLn v
 
+withFileFC :: FilePath -> (Buffer -> FC a) -> FC a
+withFileFC fp f = withFile fp ReadMode $ \h ->
+    (Buffer.fromByteString <$> lift (BS.readFile h)) >>= f
+
 execConsoleAction :: ConsoleAction -> FC ()
 execConsoleAction (Flash f)       = execFlash f
-execConsoleAction (Install b f)   = execInstall b f
+execConsoleAction (Install m f)   = execInstall m f
 execConsoleAction (Launch s)      = execLaunch s
 execConsoleAction Reset           = execReset
 execConsoleAction Suspend         = execSuspend
@@ -57,74 +62,55 @@ execInstall :: BundleID -> FilePath -> FC ()
 execInstall = undefined
 
 execLaunch :: String -> FC ()
-execLaunch = FDL.launch . FDL.bundleKey
+execLaunch = undefined
 
 execReset :: FC ()
-execReset = SAM.reset
+execReset = CPU.reset
 
 execSuspend :: FC ()
-execSuspend = SAM.suspend
+execSuspend = CPU.halt
 
 execEngage :: FC ()
-execEngage = SAM.engage
+execEngage = CPU.power True
 
 execFormat :: FC ()
-execFormat = NVM.format
+execFormat = FS.format
 
 execCall :: Call -> FC ()
 execCall (ButtonCall b) = execButtonAction b
-execCall (ConfigCall c) = execConfigAction c
-execCall (IOCall i)     = execIOAction i
+execCall (FSCall f)     = execFSAction f
+execCall (GPIOCall g)   = execGPIOAction g
 execCall (LEDCall l)    = execLEDAction l
-execCall (NVMCall n)    = execNVMAction n
 execCall (SPICall s)    = execSPIAction s
-execCall (USARTCall u)  = execUSARTAction u
-execCall (USBCall u)    = execUSBAction u
+execCall (UARTCall u)   = execUSARTAction u
 
 execButtonAction :: ButtonAction -> FC ()
 execButtonAction ButtonRead = Button.read >>= printFC
 
-execConfigAction :: ConfigAction -> FC ()
-execConfigAction (ConfigRead k)    = Config.read k >>= printFC
-execConfigAction (ConfigWrite k v) = Config.write k v
-
-execIOAction :: IOAction -> FC ()
-execIOAction (IODigitalDirection p d) = IO.digitalDirection p d
-execIOAction (IODigitalRead p)        = IO.digitalRead p >>= printFC
-execIOAction (IODigitalWrite p v)     = IO.digitalWrite p v
-execIOAction (IOAnalogDirection p d)  = IO.analogDirection p d
-execIOAction (IOAnalogRead p)         = IO.analogRead p >>= printFC
-execIOAction (IOAnalogWrite p v)      = IO.analogWrite p v
+execGPIOAction :: GPIOAction -> FC ()
+execGPIOAction (GPIODigitalDirection p d) = GPIO.digitalDirection p d
+execGPIOAction (GPIODigitalRead p)        = GPIO.digitalRead p >>= printFC
+execGPIOAction (GPIODigitalWrite p v)     = GPIO.digitalWrite p v
+execGPIOAction (GPIOAnalogDirection p d)  = GPIO.analogDirection p d
+execGPIOAction (GPIOAnalogRead p)         = GPIO.analogRead p >>= printFC
+execGPIOAction (GPIOAnalogWrite p v)      = GPIO.analogWrite p v
 
 execLEDAction :: LEDAction -> FC ()
 execLEDAction (LEDSetRGB c) = LED.setRGB c
 
-execNVMAction :: NVMAction -> FC ()
-execNVMAction NVMEnable      = NVM.enable
-execNVMAction NVMDisable     = NVM.disable
-execNVMAction NVMReset       = NVM.reset
-execNVMAction NVMFormat      = NVM.format
-execNVMAction (NVMAlloc s)   = NVM.alloc (fromIntegral s) >>= printFC
-execNVMAction (NVMRead h)    = NVM.pull h >>= printCStringFC
-execNVMAction (NVMWrite h s) = NVM.pushHandle s h
-
 execSPIAction :: SPIAction -> FC ()
-execSPIAction SPIEnable    = SPI.enable
-execSPIAction SPIDisable   = SPI.disable
-execSPIAction SPIRead      = SPI.pull >>= printCStringFC
-execSPIAction (SPIWrite s) = SPI.push s
+execSPIAction SPIEnable                 = SPI.enable
+execSPIAction SPIDisable                = SPI.disable
+execSPIAction (SPIRead i)               = SPI.pull i >>= printCStringFC
+execSPIAction (SPIWriteFromString s)    = SPI.push s
+execSPIAction (SPIWriteFromFilePath fp) = withFileFC fp SPI.push
 
-execUSARTAction :: USARTAction -> FC ()
-execUSARTAction (USARTEnable u)  = USART.enable u
-execUSARTAction (USARTDisable u) = USART.disable u
-execUSARTAction (USARTRead u)    = USART.pull u >>= printCStringFC
-execUSARTAction (USARTWrite u s) = USART.push u s
-
-execUSBAction :: USBAction -> FC ()
-execUSBAction USBEnable    = USB.enable
-execUSBAction USBDisable   = USB.disable
-execUSBAction USBRead      = USB.pull >>= printCStringFC
-execUSBAction (USBWrite s) = USB.push s
+execUARTAction :: UARTAction -> FC ()
+execUARTAction UARTEnable                 = UART.enable
+execUARTAction UARTDisable                = UART.disable
+execUARTAction (UARTRead i)               = UART.pull i >>= printCStringFC
+execUARTAction (UARTWriteFromString s)    = UART.push s
+execUARTAction (UARTWriteFromFilePath fp) = withFileFC fp UART.push
 
 fcREPL :: FC ()
 fcREPL = do
