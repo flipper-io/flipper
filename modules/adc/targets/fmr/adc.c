10aca07c3aed47cb35def7b36cf6f7075a972d66
George Morgan <george@george-morgan.com>
2016-10-19 13:11:50 -0400

    Removed dead test fragment.

--- modules/adc/targets/fmr/adc.c
+++ modules/adc/targets/fmr/adc.c
@@ -2,15 +2,6 @@
 #include <flipper/modules.h>
 #include <flipper/fmr.h>
 
-/* Declare the FMR reference for this module. */
-struct _lf_module _adc_m = {
-	"adc",
-	"Provides access to the integrated analog to digital converter.",
-	LF_VERSION,
-	_adc_id
-}, *_adc;
-
 int adc_configure(void) {
-	printf("Woo! ADCs rock!\n");
 	return lf_success;
 }
