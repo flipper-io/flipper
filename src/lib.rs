8c1b77964947a5bb8be56407a39b5c2f768d8001
Nick Mosher <nicholastmosher@gmail.com>
2017-09-15 03:22:21 -0400

    Bundle methods onto SamBa struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,19 @@
 #[macro_use]
 extern crate serde_derive;
 extern crate byteorder;
-extern crate flipper_rust;
-
+extern crate xmodem;
 extern crate serde;
 extern crate toml;
+
+extern crate flipper_rust;
+
+use std::result;
 
 pub mod packages;
 pub mod hardware;
 
+pub enum Error {
+    FileNotFound,
+}
 
+pub type Result<T> = result::Result<T, Error>;
