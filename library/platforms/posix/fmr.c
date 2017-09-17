cadba0c15d14b1e516b6f3d8ad218e7f60524f07
Nick Mosher <nicholastmosher@gmail.com>
2017-09-17 00:24:12 -0400

    Fix FVM uart0

--- library/platforms/posix/fmr.c
+++ library/platforms/posix/fmr.c
@@ -28,9 +28,9 @@ const void *const fmr_modules[] = {
 
 uint32_t fmr_call(const uint32_t (* function)(void), uint8_t argc, uint16_t argt, void *argv) {
 	/* Grab the symbol name of the function for debugging purposes. */
-	Dl_info info;
-	dladdr(function, &info);
-	printf("Calling local function '%s', with %i arguments, arg types %i, and va_list %p.\n", info.dli_sname, argc, argt, argv);
+	//Dl_info info;
+	//dladdr(function, &info);
+	//printf("Calling local function '%s', with %i arguments, arg types %i, and va_list %p.\n", info.dli_sname, argc, argt, argv);
 	return function();
 }
 
