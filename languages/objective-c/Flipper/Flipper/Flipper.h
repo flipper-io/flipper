b283a863edf1d26ea24d842b4c19954c70762c02
Nick Mosher <nicholastmosher@gmail.com>
2017-09-20 21:21:36 -0400

    Add objective-c language bindings

---
+++ languages/objective-c/Flipper/Flipper/Flipper.h
@@ -0,0 +1,20 @@
+//
+//  Flipper.h
+//  Flipper
+//
+//  Created by George Morgan on 10/27/16.
+//  Copyright © 2016 Flipper. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Flipper.
+FOUNDATION_EXPORT double FlipperVersionNumber;
+
+//! Project version string for Flipper.
+FOUNDATION_EXPORT const unsigned char FlipperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flipper/PublicHeader.h>
+#import <Flipper/LFDevice.h>
+#import <Flipper/LED.h>
+#import <Flipper/GPIO.h>
