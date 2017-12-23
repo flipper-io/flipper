a0c132d38f7be1128cfc8a1df13ebdd1a952e0c0
Nick Mosher <nicholastmosher@gmail.com>
2017-12-23 01:29:44 -0500

    Add key libflipper structs. Clean lib and uart0

--- languages/rust/src/fsm/uart0.rs
+++ languages/rust/src/fsm/uart0.rs
@@ -6,9 +6,6 @@ use ::Flipper;
 #[link(name = "flipper")]
 extern {
     fn uart0_configure() -> u32;
-    fn uart0_enable();
-    fn uart0_disable();
-    fn uart0_dfu();
     fn uart0_ready() -> u8;
     fn uart0_push(source: *const c_void, length: usize);
     fn uart0_pull(dest: *mut c_void, length: usize, timeout: u32);
@@ -16,21 +13,13 @@ extern {
 
 pub struct Uart0<'a>(&'a Flipper);
 
-impl <'a> Uart0<'a> {
+impl<'a> Uart0<'a> {
     pub fn new(flipper: &'a Flipper) -> Self {
         Uart0(flipper)
     }
 }
 
-impl <'a> ::std::ops::Deref for Uart0<'a> {
-    type Target = &'a Flipper;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl <'a> Read for Uart0<'a> {
+impl<'a> Read for Uart0<'a> {
     fn read(&mut self, buf: &mut [u8]) -> Result<usize> {
         unsafe {
             uart0_pull(buf.as_mut_ptr() as *mut c_void, buf.len(), 0xff)
@@ -39,18 +28,15 @@ impl <'a> Read for Uart0<'a> {
     }
 }
 
-impl <'a> Write for Uart0<'a> {
+impl<'a> Write for Uart0<'a> {
     fn write(&mut self, buf: &[u8]) -> Result<usize> {
-        self.0.select();
         unsafe {
             uart0_push(buf.as_ptr() as *const c_void, buf.len())
         }
         Ok(buf.len())
     }
 
-    fn flush(&mut self) -> Result<()> {
-        unimplemented!()
-    }
+    fn flush(&mut self) -> Result<()> { }
 }
 
 pub fn configure() {
@@ -59,16 +45,4 @@ pub fn configure() {
 
 pub fn ready() -> bool {
     unsafe { uart0_ready() != 0 }
-}
-
-pub fn enable() {
-    unsafe { uart0_enable() }
-}
-
-pub fn disable() {
-    unsafe { uart0_disable() }
-}
-
-pub fn dfu() {
-    unsafe { uart0_dfu() };
 }
--- languages/rust/src/lib.rs
+++ languages/rust/src/lib.rs
@@ -7,40 +7,73 @@ extern crate libc;
 pub mod fsm;
 
 use std::ffi::{CString, CStr};
-use std::str;
-use std::fmt;
-use libc::{c_char, c_void};
+use libc::{c_void, c_char, c_int};
 
+pub const LF_VERSION: u16 = 0x0001;
+
+type _fmr_arg_index = u8;
+type _fmr_return = u32;
+
+/// The libflipper native representation of a linked list. We need this
+/// representation so we can construct parameter lists for FMR invocations.
 #[repr(C)]
-struct _lf_configuration {
-    name: [c_char; 16],
-    identifier: i32,
-    version: i32,
-    attributes: i8
+#[derive(Debug)]
+struct _lf_ll {
+    item: *const c_void,
+    destructor: *const c_void,
+    next: *const _lf_ll,
 }
 
+/// The libflipper native representation of a module. An `_lf_module` struct
+/// is the most important piece of information in FMR because it's name is a
+/// unique identifier, its index maps it to a specific memory location on the
+/// device, and the device pointer specifies which Flipper this module sends
+/// calls to.
 #[repr(C)]
-struct _lf_device {
-    configuration: _lf_configuration,
-    endpoint: *const c_void,
-    error: i32
+#[derive(Debug)]
+struct _lf_module {
+    /// The name and unique identifier of this module.
+    name: *const c_char,
+    /// An optional description of what this module does.
+    description: *const c_char,
+    /// A version of this module. Important to update when APIs change so that
+    /// FMR can keep track of how to execute functions correctly.
+    version: u16,
+    /// A checksum of the module.
+    crc: u16,
+    /// The index into Flipper's module store where the native code for this
+    /// module is kept. Flipper loads modules sequentially, with the standard
+    /// modules automatically pre-loaded from 0-X.
+    index: u16,
+    /// A pointer representing the specific Flipper device on which this module's
+    /// native code lives and therefore where calls using this module will be
+    /// executed. If `NULL`, calls will automatically go to the currently-selected
+    /// Flipper, as tracked by libflipper.
+    device: *const c_void,
+}
+
+/// The public facing struct representing a Flipper Module keeps the details
+/// of module representation a secret in order to prevent breaking API changes.
+#[derive(Debug)]
+pub struct Module {
+    module: _lf_module,
 }
 
 #[link(name = "flipper")]
 extern {
-    fn flipper_attach() -> *mut _lf_device;
-    fn carbon_attach_hostname(hostname: *const c_char) -> *mut _lf_device;
-    fn flipper_select(device: *mut _lf_device) -> i32;
-    fn flipper_detach(device: *mut _lf_device) -> i32;
+    fn flipper_attach() -> *const c_void;
+    fn carbon_attach_hostname(hostname: *const c_char) -> *const c_void;
+    fn flipper_detach(device: *const c_void) -> i32;
+    fn lf_invoke(module: *const _lf_module, function: _fmr_arg_index, ret: u8, args: *const _lf_ll) -> _fmr_return;
+    fn lf_ll_append(ll: *mut *mut _lf_ll, item: *const c_void, destructor: *const c_void) -> c_int;
 }
 
 pub struct Flipper {
     /* Reference to the _lf_device struct. */
-    _device: *mut _lf_device,
+    _device: *const c_void,
 }
 
 impl Flipper {
-
     pub fn attach() -> Self {
         unsafe {
             Flipper {
@@ -56,44 +89,6 @@ impl Flipper {
             }
         }
     }
-
-    pub fn select(&self) {
-        unsafe {
-            flipper_select(self._device)
-        };
-    }
-
-    /// Returns the name of the device as a string slice.
-    pub fn name(&self) -> &str {
-        let c_buf: *const c_char = unsafe { (*self._device).configuration.name.as_ptr() };
-        let c_str: &CStr = unsafe { CStr::from_ptr(c_buf) };
-        let buf: &[u8] = c_str.to_bytes();
-        return str::from_utf8(buf).unwrap();
-    }
-
-    pub fn identifier(&self) -> i32 {
-        unsafe {
-            (*self._device).configuration.identifier
-        }
-    }
-
-    pub fn version(&self) -> i32 {
-        unsafe {
-            (*self._device).configuration.version
-        }
-    }
-
-    pub fn attributes(&self) -> i8 {
-        unsafe {
-            (*self._device).configuration.attributes
-        }
-    }
-}
-
-impl fmt::Display for Flipper {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "name: {}\nidentifier: {:#x}\nversion: {:#x}\nattributes: {:#x}", self.name(), self.identifier(), self.version(), self.attributes())
-    }
 }
 
 impl Drop for Flipper {
@@ -101,23 +96,5 @@ impl Drop for Flipper {
         unsafe {
             flipper_detach(self._device)
         };
-    }
-}
-
-mod test {
-
-    #[test]
-    fn test() {
-        /* Attach to a device. */
-        let device = attach();
-        /* Print the device info. */
-        println!("{}", device);
-        /* Configure the LED peripheral. */
-        fsm::led::configure();
-        /* Set the LED color. */
-        fsm::led::rgb(0, 10, 0);
-        /* Do some gpio stuff. */
-        fsm::gpio::enable((1 << 0), 0);
-        fsm::gpio::write((1 << 0), 0);
     }
 }
