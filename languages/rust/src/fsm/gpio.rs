f088d981cb44821e52044189f5032ed1f1609be0
Nick Mosher <nicholastmosher@gmail.com>
2018-01-13 19:37:51 -0500

    Update gpio implementation to use lf_invoke

--- languages/rust/src/fsm/gpio.rs
+++ languages/rust/src/fsm/gpio.rs
@@ -1,24 +1,69 @@
+#![allow(non_upper_case_globals)]
+
+use ::{
+    Flipper,
+    StandardModule,
+    StandardModuleFFI,
+    ModuleFFI,
+    _lf_module,
+};
+
+use fmr::{
+    Args,
+    lf_invoke,
+};
+
 #[link(name = "flipper")]
 extern {
-    fn gpio_configure() -> i32;
-    fn gpio_enable(enable: i32, disable: i32);
-    fn gpio_write(set: i32, clear: i32);
+    static _gpio: _lf_module;
 }
 
-pub fn configure() -> i32 {
-    unsafe {
-        return gpio_configure();
-    };
+pub struct Gpio {
+    module: ModuleFFI,
 }
 
-pub fn enable(enable: i32, disable: i32) {
-    unsafe {
-        gpio_enable(enable, disable);
-    };
+impl StandardModule for Gpio {
+    fn new() -> Self {
+        unsafe {
+            let module = StandardModuleFFI { module_meta: &_gpio };
+            Gpio {
+                module: ModuleFFI::Standard(module),
+            }
+        }
+    }
+
+    fn bind(flipper: &Flipper) -> Self {
+        unsafe {
+            let module = StandardModuleFFI { module_meta: &_gpio };
+            Gpio {
+                module: ModuleFFI::Standard(module),
+            }
+        }
+    }
 }
 
-pub fn write(set: i32, clear: i32) {
-    unsafe {
-        gpio_write(set, clear);
-    };
+impl Gpio {
+    pub fn configure(&self) {
+        lf_invoke(&self.module, 0, Args::new())
+    }
+
+    pub fn enable(&self, enable: u32, disable: u32) {
+        let args = Args::new()
+            .append(enable)
+            .append(disable);
+        lf_invoke(&self.module, 1, args)
+    }
+
+    pub fn write(&self, set: u32, clear: u32) {
+        let args = Args::new()
+            .append(set)
+            .append(clear);
+        lf_invoke(&self.module, 2, args)
+    }
+
+    pub fn read(&self, mask: u32) -> u32 {
+        let args = Args::new()
+            .append(mask);
+        lf_invoke(&self.module, 3, args)
+    }
 }
