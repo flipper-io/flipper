dc93fff82813a3d8cc8a61f69ea87007fcf96f43
George Morgan <george@george-morgan.com>
2018-03-12 17:42:50 -0700

    Update python bindings

--- languages/python/flipper/lf.py
+++ languages/python/flipper/lf.py
@@ -56,18 +56,6 @@ class flipper:
 		self.ref = device
 		return
 
-class module:
-	ref = None
-	def __init__(self, name, device = None, ref = None):
-		if (device == None):
-			device = flipper()
-		if (ref):
-			self.ref = ref
-		else:
-			libflipper.lf_module_create.restype = c_void_p
-			self.ref = libflipper.lf_module_create(c_char_p(name), device.ref)
-			bind(self, device)
-
 def attach():
 	libflipper.flipper_attach.restype = c_void_p
 	return libflipper.flipper_attach()
@@ -96,5 +84,5 @@ def getModule(name):
 	return module(name, None, byref(_module))
 
 def invoke(module, function, ret, arguments):
-	libflipper.lf_invoke(current_device(), module.ref, c_byte(function), c_byte(ret), lf_ll_from_list(arguments))
+	libflipper.lf_invoke(c_void_p(current_device()), c_char_p(module), c_byte(function), c_byte(ret), lf_ll_from_list(arguments))
 	return
