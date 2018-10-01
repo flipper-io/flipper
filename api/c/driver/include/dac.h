2fda4b31cdc50fddc99c656b674b72baee62b7a8
Nick Mosher <nicholastmosher@gmail.com>
2018-10-01 15:15:59 -0400

    Separate api for drivers and clients

---
+++ api/c/driver/include/dac.h
@@ -0,0 +1,7 @@
+#ifndef __dac_h__
+#define __dac_h__
+
+/* Declare the prototypes for all of the functions within this module. */
+int dac_configure(void);
+
+#endif
---
+++ api/c/driver/include/temp.h
@@ -0,0 +1,7 @@
+#ifndef __temp_h__
+#define __temp_h__
+
+/* Declare the prototypes for all of the functions within this module. */
+int temp_configure(void);
+
+#endif
